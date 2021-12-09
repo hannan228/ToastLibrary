1097c45a09cf0774cbdd7e187453f52923fc4731
hannan228 <abdulhannan1146@gmail.com>
2021-12-09 12:18:50 +0500

    initial commit

---
+++ Toaster/src/main/java/jav/app/toaster/ToastAMessage.java
@@ -0,0 +1,12 @@
+package jav.app.toaster;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastAMessage {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
